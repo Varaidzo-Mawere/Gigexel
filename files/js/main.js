d249b1e9a130ffea155ddfaa99a4f56136397975
varaidzo-mawere <varaidzo.mawere@uncommon.org>
2021-11-01 14:44:35 +0200

    first commit

---
+++ files/js/main.js
@@ -0,0 +1,208 @@
+
+
+window.onload=function(){
+    const menu_btn=document.querySelector('.hamburger');
+    const mobile_menu= document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    })
+   
+}
+
+
+
+
+
+
+
+
+
+ 
+   document.getElementById("profile1").style.display ='block';
+
+   document.getElementById("profile2").style.display ='none';
+ 
+   document.getElementById("profile3").style.display ='none';
+   document.getElementById("profile4").style.display ='none';
+   document.getElementById("profile5").style.display ='none';
+   document.getElementById("profile6").style.display ='none';
+   document.getElementById("profile7").style.display ='none';
+   document.getElementById("profile8").style.display ='none';
+   document.getElementById("profile9").style.display ='none';
+   document.getElementById("profile10").style.display ='none';
+   document.getElementById("profile11").style.display ='none';
+  
+ 
+    const myFunction2 = () => {
+       document.getElementById("profile2").style.display ='block';
+       document.getElementById("profile1").style.display ='none';
+       document.getElementById("profile3").style.display ='none';
+       document.getElementById("profile4").style.display ='none';
+       document.getElementById("profile5").style.display ='none';
+       document.getElementById("profile6").style.display ='none';
+       document.getElementById("profile7").style.display ='none';
+       document.getElementById("profile8").style.display ='none';
+       document.getElementById("profile9").style.display ='none';
+       document.getElementById("profile10").style.display ='none';
+       document.getElementById("profile11").style.display ='none';
+      
+   }
+ 
+   const myFunction1 = () => {
+     document.getElementById("profile1").style.display ='block';
+     document.getElementById("profile2").style.display ='none';
+     document.getElementById("profile3").style.display ='none';
+     document.getElementById("profile4").style.display ='none';
+     document.getElementById("profile5").style.display ='none';
+     document.getElementById("profile6").style.display ='none';
+     document.getElementById("profile7").style.display ='none';
+     document.getElementById("profile8").style.display ='none';
+     document.getElementById("profile9").style.display ='none';
+     document.getElementById("profile10").style.display ='none';
+     document.getElementById("profile11").style.display ='none';
+ }
+ 
+ 
+   const myFunction3 = () => {
+     document.getElementById("profile3").style.display ='block';
+     document.getElementById("profile1").style.display ='none';
+     document.getElementById("profile2").style.display ='none';
+     document.getElementById("profile4").style.display ='none';
+     document.getElementById("profile5").style.display ='none';
+     document.getElementById("profile6").style.display ='none';
+     document.getElementById("profile7").style.display ='none';
+     document.getElementById("profile8").style.display ='none';
+     document.getElementById("profile9").style.display ='none';
+     document.getElementById("profile10").style.display ='none';
+     document.getElementById("profile11").style.display ='none';
+     
+ }
+ 
+ const myFunction4 = () => {
+    document.getElementById("profile4").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile5").style.display ='none';
+    document.getElementById("profile6").style.display ='none';
+    document.getElementById("profile7").style.display ='none';
+    document.getElementById("profile8").style.display ='none';
+    document.getElementById("profile9").style.display ='none';
+    document.getElementById("profile10").style.display ='none';
+    document.getElementById("profile11").style.display ='none';
+    
+}
+
+
+
+const myFunction5 = () => {
+    document.getElementById("profile5").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile4").style.display ='none';
+    document.getElementById("profile6").style.display ='none';
+    document.getElementById("profile7").style.display ='none';
+    document.getElementById("profile8").style.display ='none';
+    document.getElementById("profile9").style.display ='none';
+    document.getElementById("profile10").style.display ='none';
+    document.getElementById("profile11").style.display ='none';
+    
+}
+
+
+const myFunction6 = () => {
+    document.getElementById("profile6").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile4").style.display ='none';
+    document.getElementById("profile5").style.display ='none';
+    document.getElementById("profile7").style.display ='none';
+    document.getElementById("profile8").style.display ='none';
+    document.getElementById("profile9").style.display ='none';
+    document.getElementById("profile10").style.display ='none';
+    document.getElementById("profile11").style.display ='none';
+    
+}
+const myFunction7 = () => {
+    document.getElementById("profile7").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile4").style.display ='none';
+    document.getElementById("profile5").style.display ='none';
+    document.getElementById("profile6").style.display ='none';
+    document.getElementById("profile8").style.display ='none';
+    document.getElementById("profile9").style.display ='none';
+    document.getElementById("profile10").style.display ='none';
+    document.getElementById("profile11").style.display ='none';
+    
+}
+
+const myFunction8 = () => {
+    document.getElementById("profile8").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile4").style.display ='none';
+    document.getElementById("profile5").style.display ='none';
+    document.getElementById("profile6").style.display ='none';
+    document.getElementById("profile7").style.display ='none';
+    document.getElementById("profile9").style.display ='none';
+    document.getElementById("profile10").style.display ='none';
+    document.getElementById("profile11").style.display ='none';
+    
+}
+
+const myFunction9 = () => {
+    document.getElementById("profile9").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile4").style.display ='none';
+    document.getElementById("profile5").style.display ='none';
+    document.getElementById("profile6").style.display ='none';
+    document.getElementById("profile7").style.display ='none';
+    document.getElementById("profile8").style.display ='none';
+    document.getElementById("profile10").style.display ='none';
+    document.getElementById("profile11").style.display ='none';
+    
+}
+
+const myFunction10 = () => {
+    document.getElementById("profile10").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile4").style.display ='none';
+    document.getElementById("profile5").style.display ='none';
+    document.getElementById("profile6").style.display ='none';
+    document.getElementById("profile7").style.display ='none';
+    document.getElementById("profile8").style.display ='none';
+    document.getElementById("profile9").style.display ='none';
+    document.getElementById("profile11").style.display ='none';
+    
+}
+
+
+
+const myFunction11 = () => {
+    document.getElementById("profile11").style.display ='block';
+    document.getElementById("profile1").style.display ='none';
+    document.getElementById("profile2").style.display ='none';
+    document.getElementById("profile3").style.display ='none';
+    document.getElementById("profile4").style.display ='none';
+    document.getElementById("profile5").style.display ='none';
+    document.getElementById("profile6").style.display ='none';
+    document.getElementById("profile7").style.display ='none';
+    document.getElementById("profile8").style.display ='none';
+    document.getElementById("profile9").style.display ='none';
+    document.getElementById("profile10").style.display ='none';
+    
+}
+
+
+
